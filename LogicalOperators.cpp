fdb8804207471e1d6587e8d2eaa4804c1e18c90d
nuketayev <akzhol.nuketayev@gmail.com>
2024-12-22 22:47:46 +0100

    just a little more

--- LogicalOperators.cpp
+++ LogicalOperators.cpp
@@ -1,1 +1,19 @@
- 
+ #include <iostream>
+
+ int main()
+ {
+    int temp;
+
+    std::cout << "Enter the temperature: ";
+    std::cin >> temp;
+
+    if (temp > 20 && temp < 29)
+        std::cout << "It is warm outside!\n";
+    else if (temp < -30 || temp > 40)
+        std::cout << "Do not go outside!\n";
+    else if (temp != 33)
+        std::cout << "It is not 33 degrees\n";
+    else
+        std::cout << "It's 33 degrees\n";
+    return 0;
+ }
--- TernaryOperator.cpp
+++ TernaryOperator.cpp
@@ -1,1 +1,13 @@
- 
+ #include <iostream>
+
+int main()
+{
+    int grade;
+
+    std::cout << "Enter your grade: ";
+    std::cin >> grade;
+
+    grade >= 60 ? std::cout << "You pass!\n" : std::cout << "You didn't pass!\n";
+    
+    return 0; 
+}

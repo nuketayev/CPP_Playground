1d64775286af1570cfe30f6f3bb08afef8cdbd3d
nuketayev <akzhol.nuketayev@gmail.com>
2024-12-23 23:36:31 +0100

    23 23:23

--- StringMethods.cpp
+++ StringMethods.cpp
@@ -1,1 +1,43 @@
+ #include <iostream>
  
+//www.cplusplus.com/reference/string/string/
+
+ int main()
+ {
+    std::string name;
+
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
+
+    if(name.empty())
+    {
+        std::cout << "You didn't enter your name\n";
+        return 0;
+    }
+
+    if(name.length() > 12)
+    {
+        std::cout << "Your name can't be over 12 characters long\n";
+    }
+    else
+    {
+        std::cout << "Welcome " << name << "\n";
+    }
+
+    std::cout << "Your username is now " << name.append("@gmail.com") << "\n";
+
+    std::cout << "The first character of your name is: " << name.at(0) << "\n";
+    
+    std::cout << "Nickname is: " << name.insert(0, "@") << "\n";
+
+    // name.find(' ') will find position of the first space
+
+    name.erase(0,3); //eliminate the first three characters
+
+    std::cout << "Without the first three characters: " << name << "\n";
+
+    name.clear();
+    std::cout << "Goodbye!" << name << "\n";
+
+    return 0;
+ }
--- TemperatureConversion.cpp
+++ TemperatureConversion.cpp
@@ -1,1 +1,38 @@
- 
+ #include <iostream>
+
+ int main()
+ {
+    double temp;
+    char unit;
+
+    std::cout << "****** Temperature conversion *******\n";
+    std::cout << "F - Fehrenheit\n";
+    std::cout << "C - Celsius\n";
+    std::cout << "What unit would you like to convert to: ";
+    std::cin >> unit;
+
+    if (unit == 'F' || unit == 'f')
+        {
+        std::cout << "Enter the temperature in Celsius: ";
+        std::cin >> temp;
+
+        temp = (1.8 * temp) + 32.0;
+        std::cout << "Converted temperature is: " << temp << " F\n";        
+    }
+    else if (unit == 'C' || unit == 'c')
+    {
+        std::cout << "Enter the temperature in Fehrenheit: ";
+        std::cin >> temp;
+
+        temp = (temp - 32.0) / 1.8;
+        std::cout << "Converted temperature is: " << temp << " C\n"; 
+    }
+    else
+    {
+        std::cout << "Invalid input";
+    }
+
+    std::cout << "**************************************\n";
+
+    return 0;
+ }

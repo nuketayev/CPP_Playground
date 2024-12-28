1fc78eea6179f784068d483f674ad371ec1d9af3
nuketayev <akzhol.nuketayev@gmail.com>
2024-12-28 18:52:32 +0100

    nullptr

--- NullPointers.cpp
+++ NullPointers.cpp
@@ -2,5 +2,22 @@
 
 int main()
 {
-    
+    int *pointer = nullptr;
+
+    int x = 123;
+
+    // pointer = &x;
+
+    if(pointer == nullptr)
+    {
+        std::cout << "Address was not assigned and its ocupied!\n";
+        std::cout << *pointer << '\n';
+    }
+    else
+    {
+        std::cout << "Addsress was assigned!\n";
+        std::cout << *pointer << '\n';
+    }
+
+    return 0;
 }
---
+++ TicTacToe.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+void drawBoard(char *spaces)
+{
+    
+
+    return 0;
+}
+
+void playerMove(char *spaces)
+{
+
+    return 0;
+}
+
+void computerMove(char *spaces, char player)
+{
+
+    return 0;
+}
+
+bool checkWinner(char *spaces, char player, char computer)
+{
+    return 0;
+}
+
+bool checkTie(char *spaces)
+{
+
+    return 0;
+}
+
+int main()
+{
+    char spaces[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+    char player = 'X';
+    char computer = 'O';
+    bool running = true;
+
+    drawBoard(spaces);
+    
+
+    return 0;
+}

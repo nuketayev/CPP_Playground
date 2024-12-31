c8c7604bf3facd31f9ecae255dfcd5d45db99dd4
nuketayev <akzhol.nuketayev@gmail.com>
2024-12-31 18:22:55 +0100

    inheritance in cpp

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -1,49 +1,91 @@
 #include <iostream>
 
-class animal
+class Shape
 {
     public:
-        bool alive = true;
-    void eat()
-    {
-        std::cout << "This animal is eating\n";
-    }
+        double area;
+        double volume;
 };
 
-class Dog : public animal
+class Cube : public Shape
 {
     public:
-        void bark()
+        double side;
+        Cube(double side)
         {
-            std::cout << "The dog does woof!\n";
+            this->side = side;
+            this->area = 6 * side * side;
+            this->volume = side * side * side;
         }
 };
 
-class Cat : public animal
+class Sphere : public Shape
 {
     public:
-
-    void meow()
-    {
-        std::cout << "The cat goes meow!\n";
-    }
+        double radius;
+        Sphere(double radius)
+        {
+            this->radius = radius;
+            this->area = 4 * 3.14 * radius * radius;
+            this->volume = 4/3 * 3.14 * radius * radius * radius;
+        }
 };
 
 int main()
 {
+    Cube cube(5);
+    Sphere sphere(5);
 
-    Dog dog;
-    Cat cat;
-
-    std::cout << dog.alive << '\n';
-    dog.eat();
-    dog.bark();
-
-    std::cout << cat.alive << '\n';
-    cat.eat();
-    cat.meow();
-    
-
+    std::cout << "Sphere area: " << sphere.area << '\n';
+    std::cout << "Sphere volume: " << sphere.volume << '\n';
 
     return 0;
 }
+
+// class animal
+// {
+//     public:
+//         bool alive = true;
+//     void eat()
+//     {
+//         std::cout << "This animal is eating\n";
+//     }
+// };
+
+// class Dog : public animal
+// {
+//     public:
+//         void bark()
+//         {
+//             std::cout << "The dog does woof!\n";
+//         }
+// };
+
+// class Cat : public animal
+// {
+//     public:
+
+//     void meow()
+//     {
+//         std::cout << "The cat goes meow!\n";
+//     }
+// };
+
+// int main()
+// {
+
+//     Dog dog;
+//     Cat cat;
+
+//     std::cout << dog.alive << '\n';
+//     dog.eat();
+//     dog.bark();
+
+//     std::cout << cat.alive << '\n';
+//     cat.eat();
+//     cat.meow();
+    
+
+
+//     return 0;
+// }
